ac9e3db84a12fcc8839f217b2f3acba6b27bc412
larshjelen <larshjelen@gmail.com>
2020-09-08 15:03:07 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        String romanNumeral = numberToRoman(2999);
+        System.out.println(romanNumeral);
+    }
+
+    public static String numberToRoman(int num) {
+
+            String[] thousands = {"", "M", "MM", "MMM"};
+            String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+            String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+            String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+            return thousands[num / 1000] +
+                    hundreds[(num % 1000) / 100] +
+                    tens[(num % 100) / 10] +
+                    units[num % 10];
+        }
+
+    }
+
+
